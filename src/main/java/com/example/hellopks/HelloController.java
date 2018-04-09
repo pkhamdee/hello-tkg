7d3a7010ee32054af53b7a998f8c51807ad7774d
Toshiaki Maki <tmaki@pivotal.io>
2018-04-09 18:50:47 +0900

    Add json format

--- src/main/java/com/example/hellopks/HelloController.java
+++ src/main/java/com/example/hellopks/HelloController.java
@@ -1,16 +1,19 @@
 package com.example.hellopks;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 @Controller
 public class HelloController {
-    @GetMapping(path = "/")
+    @GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
     public String index(Model model) {
         Map<String, String> env = System.getenv();
         Map<String, String> node = env.entrySet().stream() //
@@ -28,5 +31,25 @@ public class HelloController {
         model.addAttribute("env", new TreeMap<>(env));
         model.addAttribute("props", new TreeMap<>(System.getProperties()));
         return "index";
+    }
+
+    @ResponseBody
+    @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Map<String, Object> json(Model model) {
+        Map<String, String> env = System.getenv();
+        Map<String, String> node = env.entrySet().stream() //
+                .filter(e -> e.getKey().startsWith("NODE")) //
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k, v) -> v, TreeMap::new));
+        Map<String, String> pod = env.entrySet().stream() //
+                .filter(e -> e.getKey().startsWith("POD")) //
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k, v) -> v, TreeMap::new));
+        Map<String, String> container = env.entrySet().stream() //
+                .filter(e -> e.getKey().startsWith("CONTAINER")) //
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k, v) -> v, TreeMap::new));
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("node", node);
+        body.put("pod", pod);
+        body.put("container", container);
+        return body;
     }
 }
